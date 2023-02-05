c9fafe51ab2c4f57652e88f282c841da00aa7018
Juan Cardoso <juancardosods@gmail.com>
2023-02-05 17:27:54 -0300

    projeto03 add

---
+++ projeto03/aluraFlix.js
@@ -0,0 +1,8 @@
+var listaManga = ["https://i0.wp.com/blogbbm.com/wp-content/uploads/2021/02/Kaiju-01.jpg?resize=420%2C659&ssl=1",
+    "https://m.media-amazon.com/images/I/51BeRXEKuWL.jpg",
+    "https://m.media-amazon.com/images/I/91D07epNE9L.jpg",
+    "https://m.media-amazon.com/images/I/51laLJP8XmL.jpg"]
+
+for (i = 0; i < listaManga.length; i++) {
+    document.write('<img src=' + listaManga[i] + '>')
+}
